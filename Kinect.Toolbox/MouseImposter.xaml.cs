e7f3ef96119ecd68438c33d993d53565d18cd5dc
dgo721 <dgo2389@hotmail.com>
2013-09-29 13:23:19 -0500

    First commit

---
+++ Kinect.Toolbox/MouseImposter.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Kinect.Toolbox
+{
+    /// <summary>
+    /// Interaction logic for MouseImposter.xaml
+    /// </summary>
+    public partial class MouseImposter : UserControl
+    {
+        public event Action OnProgressionCompleted;
+        
+        public MouseImposter()
+        {
+            InitializeComponent();
+        }
+
+        public int Progression
+        {
+            set
+            {
+                if (value == 0 || value > 100)
+                {
+                    progressBar.Visibility = System.Windows.Visibility.Collapsed;
+                    if (value > 100 && OnProgressionCompleted != null)
+                        OnProgressionCompleted();
+                }
+                else
+                {
+                    progressBar.Visibility = System.Windows.Visibility.Visible;
+                    progressBar.Value = value;
+                }
+            }
+        }
+    }
+}
